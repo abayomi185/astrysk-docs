18676e2755edd574c60c8ef2e0e85faae7baaba9
Yomi Ikuru <captyomjnr@gmail.com>
2023-06-26 20:48:40 +0100

    Change baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,8 @@ const config = {
   url: "https://abayomi185.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/astrysk-docs/",
+  // baseUrl: "/astrysk-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
